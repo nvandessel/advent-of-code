cc32e7df2e7d3943ce77d90a0ba9d40529169bf2
nic van dessel <51134175+nvandessel@users.noreply.github.com>
2022-12-02 17:19:46 +0000

    Basic dotnet setup

---
+++ adventofcode/2022/Day01/Solution.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace adventofcode.Y2022.D01
+{
+    public class Solution
+    {
+        public static void Solve()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ adventofcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventofcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Y2022.D01.Solution.Solve();
+        }
+    }
+}
